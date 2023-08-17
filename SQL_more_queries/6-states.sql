ffefcf17a544dfd2a85904f6a5d834ececa853ac
Emmanuel047 <emwendwa007@gmail.com>
2023-08-17 10:09:36 +0300

    States

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,5 +1,6 @@
 -- States Table
 CREATE SCHEMA IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states (
     id INT NOT NULL AUTOINCREMENT UNIQUE,
     name VARCHAR(256) NOT NULL
