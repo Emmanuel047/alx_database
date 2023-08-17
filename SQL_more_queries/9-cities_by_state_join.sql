7f3dac55b9074d8a322babd13a84bf49b7ea5461
Emmanuel047 <emwendwa007@gmail.com>
2023-08-17 12:46:02 +0300

    Cities by states

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- Cities by states
 SELECT cities.id, cities.name, states.name
-FROM cities, 
-LEFT JOIN states ON state.id = cities.state.id
+FROM cities
+LEFT JOIN states ON states.id = cities.state_id
 ORDER BY cities.id;
