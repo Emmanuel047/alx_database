e2398372f4c4550a658d72eb6a20bf89b8c750a1
Emmanuel047 <emwendwa007@gmail.com>
2023-08-17 10:15:58 +0300

    Cities table

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,7 @@
+-- Cities table
+CREATE SCHEMA IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities (
+    id INT AUTO_INCREMENT NOT NULL UNIQUE,
+     name VARCHAR(256)
+);
